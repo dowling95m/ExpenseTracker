d67290760872ca61458930a68ab9b3c9c1ce5648
Matthew <dowling95m@comcast.net>
2024-11-21 14:08:44 -0800

    Completed Node JS steps 5 and 6 (Reading and Adding)

--- main/app.js
+++ main/app.js
@@ -4,7 +4,13 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 9144;                 // Set a port number at the top so it's easy to change in the future
+
+// app.js - SETUP section
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+PORT        = 9164;                 // Set a port number at the top so it's easy to change in the future
 
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');
@@ -19,12 +25,64 @@ var db = require('./database/db-connector')
 */
 
 app.get('/', function(req, res)
-    {
-        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
-    });                                         // will process this file, before sending the finished HTML to the client.
+    {  
+        let query1 = "SELECT userID, userName, monthlyBudget FROM Users;";    // Define our query
 
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
+
+app.post('/add-user-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
     
-/*
+        // Capture NULL values
+        let monthlyBudget = parseInt(data.monthlyBudget);
+        if (isNaN(monthlyBudget))
+        {
+            monthlyBudget = 'NULL'
+        }
+    
+        `INSERT INTO Users (userName, monthlyBudget) VALUES ('${data.userName}', ${data.monthlyBudget})`;
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Users (userName, monthlyBudget) VALUES ('${data.userName}', ${data.monthlyBudget})`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM Users;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
+
+
+    /*
     LISTENER
 */
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
