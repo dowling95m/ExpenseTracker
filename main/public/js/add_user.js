10c95d58323afec84347d7655b041739c111c95c
Matthew <dowling95m@comcast.net>
2024-11-21 19:03:22 -0800

    Added Update! - also does not dynamically update

--- main/public/js/add_user.js
+++ main/public/js/add_user.js
@@ -92,4 +92,12 @@ addRowToTable = (data) => {
     
     // Add the row to the table
     currentTable.appendChild(row);
+
+    // Find drop down menu, create a new option, fill data in the option (full name, id),
+    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
+    let selectMenu = document.getElementById("mySelect");
+    let option = document.createElement("option");
+    option.text = newRow.userName;
+    option.value = newRow.userID;
+    selectMenu.add(option);
 }
