252a7c7618432500f46bc31faa2f1059ff0af0d0
Carter Nelson <nelscart@flip3.engr.oregonstate.edu>
2024-11-25 09:57:23 -0800

    Added comments citing sources,

--- main/app.js
+++ main/app.js
@@ -1,3 +1,8 @@
+// Citation for the following file as well as all handlebars and JS files used in the Node JS guide
+// Date: 11/21/2024
+// Adapted from the guide code, plugged in project specifics and will eventually keep expanding to make it look better
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%200%20-%20Setting%20Up%20Node.js
+
 /*
     SETUP
 */
